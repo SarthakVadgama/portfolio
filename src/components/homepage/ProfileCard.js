49295e7d4067743e6e32d6e5372cc81d410558dd
Sarthak <43034995+SarthakVadgama@users.noreply.github.com>
2023-04-07 01:10:52 +0530

    rolled back tilt

--- src/components/homepage/ProfileCard.js
+++ src/components/homepage/ProfileCard.js
@@ -17,8 +17,6 @@ function Tilt(props) {
 }
 
 const ProfileCard = () => {
-  const mediaQuery = window.matchMedia("(max-width: 768px)");
-
   const defaultOptions = {
     max: 7,
     scale: 1,
@@ -29,66 +27,15 @@ const ProfileCard = () => {
     <>
       <div className="container mt-3 pt-5">
         <center className="my-5">
-          {!mediaQuery.matches ? (
-            <Tilt
-              options={defaultOptions}
-              style={{
-                zIndex: "4",
-                position: "relative",
-                width: "auto",
-                height: "auto",
-              }}
-            >
-              <div className={`card cardBlur`} style={{ width: "100%" }}>
-                <div className={`row nogutters`}>
-                  <div className="col col-md-4">
-                    <span className={`DPbg`}>
-                      <LazyLoadImage
-                        src={require("../../assets/Dp (2).jpg")}
-                        className={`card-img-top m-3 DP`}
-                        alt="..."
-                        effect="blur"
-                        placeholdersrc={require("../../assets/Dp (2).jpg")}
-                      />
-                    </span>
-                    <LazyLoadImage
-                      src={require("../../assets/Dp (2).jpg")}
-                      className={`card-img-top m-3 DP`}
-                      alt="..."
-                      effect="blur"
-                      placeholdersrc={require("../../assets/Dp (2).jpg")}
-                    />
-                  </div>
-                  <div
-                    className="col col-md-6 card-body"
-                    style={{ cursor: "default" }}
-                  >
-                    <h2
-                      className="card-title margin mt-2"
-                      style={{ cursor: "default" }}
-                    >
-                      Sarthak Vadgama
-                    </h2>
-                    <p
-                      className={`card-text margin my-3 fs-5`}
-                      style={{ cursor: "default" }}
-                    >
-                      I am a full stack web developer with experience in
-                      building and maintaining web applications using modern
-                      technologies such as React, Node.js, MongoDB, Express,
-                      CSS3, Bootstrap, TailwindCSS etc. I have a passion for
-                      creating user-friendly, responsive, and scalable websites
-                      that solve real-world problems. I enjoy learning new
-                      skills and exploring new challenges in web development.
-                    </p>
-                    <a className="btn btn-light" href={Pdf} download>
-                      Download my Resume
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </Tilt>
-          ) : (
+          <Tilt
+            options={defaultOptions}
+            style={{
+              zIndex: "4",
+              position: "relative",
+              width: "auto",
+              height: "auto",
+            }}
+          >
             <div className={`card cardBlur`} style={{ width: "100%" }}>
               <div className={`row nogutters`}>
                 <div className="col col-md-4">
@@ -137,7 +84,7 @@ const ProfileCard = () => {
                 </div>
               </div>
             </div>
-          )}
+          </Tilt>
         </center>
       </div>
     </>
